57be8e85ee5604bb5b99c4abe30d323ba6021940
Artemiy Mazur <artemiymazur@yandex.ru>
2025-03-21 17:23:06 +0300

    Initial commit

---
+++ src/test/java/ru/netology/delivery/test/AppReplanDeliveryTest.java
@@ -0,0 +1,72 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeOptions;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+class DeliveryTest {
+
+    private DataGenerator.UserInfo validUser;
+
+    @BeforeEach
+    void setup() {
+        Configuration.browserCapabilities = new ChromeOptions().setBrowserVersion("115");
+        open("http://localhost:9999");
+         DataGenerator dataGenerator = new DataGenerator();
+    }
+
+    @Test
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        fillAndSubmitForm(firstMeetingDate);
+        checkSuccessNotification(firstMeetingDate);
+
+        updateAndSubmitForm(secondMeetingDate);
+        confirmReplanning();
+        checkSuccessNotification(secondMeetingDate);
+    }
+
+    private void fillAndSubmitForm(String meetingDate) {
+        $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(meetingDate);
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+    }
+
+    private void updateAndSubmitForm(String newDate) {
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(newDate);
+        $(".button").click();
+    }
+
+    private void checkSuccessNotification(String date) {
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + date));
+    }
+
+    private void confirmReplanning() {
+        $("[data-test-id=replan-notification] .notification__content")
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $("[data-test-id=replan-notification] button").click();
+    }
+}
