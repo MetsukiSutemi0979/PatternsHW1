880a6eeaa5182429322a51265f11f98521505f2f
MetsukiSutemi0979 <artemiymazur@yandex.ru>
2025-04-25 23:26:04 +0300

    Update import

--- src/test/java/ru/netology/delivery/test/AppReplanDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/AppReplanDeliveryTest.java
@@ -2,9 +2,9 @@ package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeOptions;
 import ru.netology.delivery.data.DataGenerator;
@@ -15,14 +15,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
-class DeliveryTest {
+class AppReplanDeliveryTest {
 
     private DataGenerator.UserInfo validUser;
 
     @BeforeEach
     void setup() {
+        Configuration.browserCapabilities = new ChromeOptions().setBrowserVersion("115");
         open("http://localhost:9999");
         validUser = DataGenerator.Registration.generateUser("ru");
+
     }
 
     @Test
